a84648b1995ff21c8832448ddececfcfbbd17785
shin-ai <muhammad.rifanny15@gmail.com>
2025-01-07 08:43:20 +0700

    add file Day01

---
+++ Day01.java
@@ -0,0 +1,33 @@
+public class Day01 {
+
+    public static void main(String[] args) {
+        System.out.println("testing");
+
+        byte tyniNumber = 1;
+        short smallNumber = 2;
+        int mediumNumber = 3;
+        long largeNumber = 4;
+
+        float mediumDecimal = 1.5f;
+        double largeDecimal = 2.5;
+
+        System.out.println(tyniNumber);
+        System.out.println(smallNumber);
+        System.out.println(mediumNumber);
+        System.out.println(largeNumber);
+
+        System.out.println(mediumDecimal);
+        System.out.println(largeDecimal);
+
+        boolean truty = true;
+        boolean falsy = false;
+
+        System.out.println(truty);
+        System.out.println(falsy);
+
+        Integer mediumObjectNumber = 5;
+
+        String word = "Sepa";
+        System.out.println(word.charAt(3));
+    }
+}
